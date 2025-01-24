17fbf092ff0964d12963f1fc969eb31b80087e3b
ABHIJEET VISHWAKARMA <abhijeetdrv@gmail.com>
2025-01-24 18:07:14 +0530

    fixed issues

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 export default function Home() {
   const router  = useRouter();
   useEffect(()=>{
-    router.push('/login')
+    router.push('/login');
   },[]);
   return (
     <>
