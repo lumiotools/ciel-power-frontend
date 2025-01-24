ccef02e35057a219be51f1867c0b1e5ca6e26997
ABHIJEET VISHWAKARMA <abhijeetdrv@gmail.com>
2025-01-24 18:04:16 +0530

    Fixed RedirectPage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router  = useRouter();
+  useEffect(()=>{
+    router.push('/login')
+  },[]);
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      Hello
-    </div>
+    <>
+    
+    </>
   );
 }
