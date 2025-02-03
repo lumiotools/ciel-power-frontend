1921359e17759b3b7d5ed6014a83c2aa2708c4f8
ABHIJEET VISHWAKARMA <abhijeetdrv@gmail.com>
2025-02-03 19:12:00 +0530

    fixed changes

--- components/component/ServiceCard.tsx
+++ components/component/ServiceCard.tsx
@@ -37,7 +37,7 @@ const ServiceCard: React.FC<ServiceCardProps> = ({ service }) => (
       style={{ backgroundColor: "#F0F8E6" }}
     >
       {/* Reduced height for content section */}
-      <CardTitle className="mt-4 line-clamp-1 text-ellipsis leading-5 text-[12px]">{service.name}</CardTitle>
+      <CardTitle className="mt-4 line-clamp-1 text-ellipsis leading-5 text-[14px]">{service.name}</CardTitle>
       <CardDescription
         className="mt-4 line-clamp-2 text-ellipsis text-[10px] font-medium"
         dangerouslySetInnerHTML={{ __html: service.description }}
