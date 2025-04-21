6e5bb99885035c5e6fd0197f6f0d13a9eebca347
DawoodKhatri <dawoodkhatri18@gmail.com>
2025-04-21 21:55:43 +0530

    FIxed Font Subset

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import AIChatbot from "@/components/chatbot/chatbot";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
